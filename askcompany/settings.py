d9e62f15cd6022d9c2e514bb591a5e1e1cb38c4d
xman2083 <xman2083@naver.com>
2019-02-12 12:38:19 +0900

    heroku

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jrt^pylq=9ip=vr5=v(=zbd^=z7ts14y6z=%e$f2zeq@e$k=o#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
